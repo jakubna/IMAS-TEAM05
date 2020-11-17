0d121b620191305ae91ab66bf809990c01b07807
jakubna <nadziejanatalia@gmail.com>
2020-11-17 19:33:09 +0100

    Registration with the DF

--- src/FzzyAgent.java
+++ src/FzzyAgent.java
@@ -7,22 +7,44 @@ import jade.lang.acl.MessageTemplate;
 import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.util.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class FzzyAgent extends Agent {
-    @Override
+    private Logger logger = Logger.getMyLogger(getClass().getName());
+    private class FuzzyAgentBehaviour extends OneShotBehaviour {
+        @Override
+        public void action() {
+            Object[] args = getArguments();
+            String name = args[0].toString();
+            System.out.println("I EXIST, I'M "+getName()+" WITH CONFIGURATION "+name);
+        }
+
+
+    }
     protected void setup() {
-        super.setup();
-        addBehaviour(new OneShotBehaviour() {
-            @Override
-            public void action() {
-                Object[] args = getArguments();
-                String name = args[0].toString();
-                System.out.println("I EXIST, I'M "+getName()+" WITH CONFIGURATION "+name);
-            }
-        });
+        // Registration with the DF
+        DFAgentDescription dfd = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("FuzzyAgent");
+        sd.setName(getName());
+        // sd.setOwnership("TEAM05");
+        dfd.setName(getAID());
+        dfd.addServices(sd);
+
+        try {
+            DFService.register(this, dfd);
+            addBehaviour(new UserAgentServeLoop());
+        } catch (FIPAException e) {
+            logger.log(Logger.SEVERE, "Agent "+getLocalName()+" - Cannot register with DF", e);
+            doDelete();
+        }
     }
 }
--- src/MngrAgent.java
+++ src/MngrAgent.java
@@ -6,65 +6,87 @@ import jade.lang.acl.MessageTemplate;
 import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 
+import jade.util.Logger;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MngrAgent extends Agent {
-    @Override
-    protected void setup() {
-        super.setup();
-        addBehaviour(new CyclicBehaviour() {
-            AID user = new AID("user", AID.ISLOCALNAME);
-            List<String> fuzzyAgents = new ArrayList<String>();
-            @Override
-            public void action() {
-                ACLMessage msg = blockingReceive(MessageTemplate.MatchSender(user));
+    private Logger logger = Logger.getMyLogger(getClass().getName());
 
-                if(msg!=null) {
-                    String cnt = msg.getContent();
-                    String type = cnt.substring(0, 2);
-                    String file = cnt.substring(2);
-                    System.out.println("manager received: " + msg.getContent());
+    private class MngrAgentServeLoop extends CyclicBehaviour {
+        AID user = new AID("user", AID.ISLOCALNAME);
+        List<String> fuzzyAgents = new ArrayList<String>();
+        @Override
+        public void action() {
+            ACLMessage msg = blockingReceive(MessageTemplate.MatchSender(user));
 
-                    if (type.equals("I_")) {
-                        // Initialize instruction
-                        // Read settings
-                        SimSettings conf = SimSettings.fromXML(file);
-                        // Fuzzy agents creation
-                        String[] fuzzyS = conf.getFuzzySettings();
-                        ContainerController container = getContainerController();
-                        for (int i=0;i<conf.getFuzzyagents();i++)
-                        {
-                            String name = "FS"+i+fuzzyS[i];
-                            if (!fuzzyAgents.contains(name)) {
-                                Object[] args = new Object[1];
-                                args[0] = fuzzyS[i];
-                                try {
-                                    AgentController a = container.createNewAgent(name, "FzzyAgent", args);
-                                    a.start();
-                                    //a.activate();
-                                    fuzzyAgents.add(name);
-                                } catch (StaleProxyException e) {
-                                    e.printStackTrace();
-                                }
-                            }else{
-                                System.out.println(name+" already exists");
+            if(msg!=null) {
+                String cnt = msg.getContent();
+                String type = cnt.substring(0, 2);
+                String file = cnt.substring(2);
+                System.out.println("manager received: " + msg.getContent());
+
+                if (type.equals("I_")) {
+                    // Initialize instruction
+                    // Read settings
+                    SimSettings conf = SimSettings.fromXML(file);
+                    // Fuzzy agents creation
+                    String[] fuzzyS = conf.getFuzzySettings();
+                    ContainerController container = getContainerController();
+                    for (int i=0;i<conf.getFuzzyagents();i++)
+                    {
+                        String name = "FS"+i+fuzzyS[i];
+                        if (!fuzzyAgents.contains(name)) {
+                            Object[] args = new Object[1];
+                            args[0] = fuzzyS[i];
+                            try {
+                                AgentController a = container.createNewAgent(name, "FzzyAgent", args);
+                                a.start();
+                                //a.activate();
+                                fuzzyAgents.add(name);
+                            } catch (StaleProxyException e) {
+                                e.printStackTrace();
                             }
+                        }else{
+                            System.out.println(name+" already exists");
                         }
-                    } else if (type.equals("D_")) {
-                        // request instruction
-                        // - contractnet
-                        System.out.println();
                     }
-
-                    // response
-                    msg = new ACLMessage(ACLMessage.INFORM);
-                    msg.addReceiver(user);
-                    msg.setContent("manager response");
-                    send(msg);
+                } else if (type.equals("D_")) {
+                    // request instruction
+                    // - contractnet
+                    System.out.println();
                 }
+
+                // response
+                msg = new ACLMessage(ACLMessage.INFORM);
+                msg.addReceiver(user);
+                msg.setContent("manager response");
+                send(msg);
             }
-        });
+        }
+    }
+
+    protected void setup() {
+        // Registration with the DF
+        DFAgentDescription dfd = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("ManagerAgent");
+        sd.setName(getName());
+        // sd.setOwnership("TEAM05");
+        dfd.setName(getAID());
+        dfd.addServices(sd);
+
+        try {
+            DFService.register(this, dfd);
+            addBehaviour(new MngrAgentServeLoop());
+        } catch (FIPAException e) {
+            logger.log(Logger.SEVERE, "Agent "+getLocalName()+" - Cannot register with DF", e);
+            doDelete();
+        }
     }
 }
