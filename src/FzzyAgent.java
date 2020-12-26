d65b58cd054b265d0e158fdaff3f52356e836c92
mike <miquelbuxonsvives@gmail.com>
2020-12-26 13:51:37 +0100

    Update contractnet, no functional

--- src/FzzyAgent.java
+++ src/FzzyAgent.java
@@ -1,15 +1,16 @@
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.FIPAAgentManagement.*;
+import jade.domain.FIPANames;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
 import jade.domain.DFService;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
-import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 import jade.util.Logger;
 
@@ -17,18 +18,62 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.ContractNetResponder;
+
 public class FzzyAgent extends Agent {
     private Logger logger = Logger.getMyLogger(getClass().getName());
-    private class FuzzyAgentBehaviour extends OneShotBehaviour {
-        @Override
-        public void action() {
+    private class FuzzyAgentBehaviour extends ContractNetResponder{
+        public FuzzyAgentBehaviour(Agent a, MessageTemplate mt) {
+            super(a, mt);
             Object[] args = getArguments();
             String name = args[0].toString();
-            System.out.println("I EXIST, I'M "+getName()+" WITH CONFIGURATION "+name);
+            System.out.println("-> "+myAgent.getName()+": Has been created with configuration "+name);
         }
 
+        @Override
+        protected ACLMessage handleCfp(ACLMessage cfp) throws RefuseException, FailureException, NotUnderstoodException {
+            System.out.println("Agent "+getLocalName()+": CFP received from "+cfp.getSender().getName()+". Action is "+cfp.getContent());
+            int proposal = (int) (Math.random() * 10);
+            if (proposal > 2) {
+                // We provide a proposal
+                System.out.println("Agent "+getLocalName()+": Proposing "+proposal);
+                ACLMessage propose = cfp.createReply();
+                propose.setPerformative(ACLMessage.PROPOSE);
+                propose.setContent(String.valueOf(proposal));
+                return propose;
+            }
+            else {
+                // We refuse to provide a proposal
+                System.out.println("Agent "+getLocalName()+": Refuse");
+                throw new RefuseException("evaluation-failed");
+            }
+        }
 
+        @Override
+        protected ACLMessage handleAcceptProposal(ACLMessage cfp, ACLMessage propose, ACLMessage accept) throws FailureException {
+            System.out.println("Agent "+getLocalName()+": Proposal accepted");
+            //codicio temporal
+            if (cfp != null) {
+                System.out.println("Agent "+getLocalName()+": Action successfully performed");
+                ACLMessage inform = accept.createReply();
+                inform.setPerformative(ACLMessage.INFORM);
+                return inform;
+            }
+            else {
+                System.out.println("Agent "+getLocalName()+": Action execution failed");
+                throw new FailureException("unexpected-error");
+            }
+        }
+
+        @Override
+        protected void handleRejectProposal(ACLMessage cfp, ACLMessage propose, ACLMessage reject) {
+            System.out.println("Agent "+getLocalName()+": Proposal rejected");
+        }
     }
+
+
     protected void setup() {
         // Registration with the DF
         DFAgentDescription dfd = new DFAgentDescription();
@@ -39,9 +84,13 @@ public class FzzyAgent extends Agent {
         dfd.setName(getAID());
         dfd.addServices(sd);
 
+        MessageTemplate template = MessageTemplate.and(
+                MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET),
+                MessageTemplate.MatchPerformative(ACLMessage.CFP) );
+
         try {
             DFService.register(this, dfd);
-            addBehaviour(new FuzzyAgentBehaviour());
+            addBehaviour(new FuzzyAgentBehaviour(this, template));
         } catch (FIPAException e) {
             logger.log(Logger.SEVERE, "Agent "+getLocalName()+" - Cannot register with DF", e);
             doDelete();
--- src/MngrAgent.java
+++ src/MngrAgent.java
@@ -1,6 +1,8 @@
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.ParallelBehaviour;
+import jade.domain.FIPANames;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.wrapper.AgentController;
@@ -12,11 +14,65 @@ import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 
 import jade.util.Logger;
-import java.util.ArrayList;
-import java.util.List;
 
-public class MngrAgent extends Agent {
+import java.util.*;
+
+import jade.lang.acl.ACLMessage;
+import jade.proto.ContractNetInitiator;
+
+public class  MngrAgent extends Agent {
     private Logger logger = Logger.getMyLogger(getClass().getName());
+    private int nResponders = 0;
+
+    private class MnrAgentContractNet extends ContractNetInitiator{
+        public MnrAgentContractNet(Agent a, ACLMessage cfp) {
+            super(a, cfp);
+            System.out.println("Content: "+cfp.getAllReceiver());
+        }
+
+        protected void handlePropose(ACLMessage propose, Vector v) {
+            System.out.println("Agent "+propose.getSender().getName()+" proposed "+propose.getContent());
+        }
+
+        protected void handleRefuse(ACLMessage refuse) {
+            System.out.println("Agent "+refuse.getSender().getName()+" refused");
+        }
+
+        protected void handleFailure(ACLMessage failure) {
+            if (failure.getSender().equals(myAgent.getAMS())) {
+                // FAILURE notification from the JADE runtime: the receiver does not exist
+                System.out.println("Responder does not exist");
+            }
+            else {
+                System.out.println("Agent "+failure.getSender().getName()+" failed");
+            }
+            // Immediate failure --> we will not receive a response from this agent
+            nResponders--;
+        }
+
+        protected void handleAllResponses(Vector responses, Vector acceptances) {
+            if (responses.size() < nResponders) {
+                // Some responder didn't reply within the specified timeout
+                System.out.println("Timeout expired: missing "+(nResponders - responses.size())+" responses");
+            }
+            // Evaluate proposals.
+            Enumeration e = responses.elements();
+            while (e.hasMoreElements()) {
+                ACLMessage msg = (ACLMessage) e.nextElement();
+                if (msg.getPerformative() == ACLMessage.PROPOSE) {
+                    ACLMessage reply = msg.createReply();
+                    reply.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+                    acceptances.addElement(reply);
+                    int proposal = Integer.parseInt(msg.getContent());
+                    System.out.println("Accepting proposal "+proposal+" from responder "+msg.getSender());
+                }
+            }
+        }
+
+        protected void handleInform(ACLMessage inform) {
+            System.out.println("Agent "+inform.getSender().getName()+" successfully performed the requested action");
+        }
+    }
 
     private class MngrAgentServeLoop extends CyclicBehaviour {
         AID user = new AID("user", AID.ISLOCALNAME);
@@ -49,6 +105,7 @@ public class MngrAgent extends Agent {
                                 a.start();
                                 //a.activate();
                                 fuzzyAgents.add(name);
+                                nResponders++;
                             } catch (StaleProxyException e) {
                                 e.printStackTrace();
                             }
@@ -57,9 +114,33 @@ public class MngrAgent extends Agent {
                         }
                     }
                 } else if (type.equals("D_")) {
+                    ACLMessage msg2 = new ACLMessage(ACLMessage.CFP);
+                    ServiceDescription sd = new ServiceDescription();
+                    DFAgentDescription dfd = new DFAgentDescription();
+                    sd.setType("FuzzyAgent");
+                    dfd.addServices(sd);
+                    try {
+                        DFAgentDescription[] result = DFService.search(myAgent, dfd);
+                        for (int i = 0; i < result.length; i++){
+                            System.out.println(result[i].getName());
+                            msg2.addReceiver(result[i].getName());
+                        }
+                    } catch (FIPAException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println(msg2.getAllReceiver());
                     // request instruction
-                    // - contractnet
-                    System.out.println();
+                    msg2.setProtocol(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET);
+                    // We want to receive a reply in 10 secs
+                    msg2.setReplyByDate(new Date(System.currentTimeMillis() + 10000));
+                    msg2.setContent("try-action");
+
+                    MnrAgentContractNet ax = new MnrAgentContractNet (myAgent, msg2);
+                    //ParallelBehaviour par = new ParallelBehaviour(ParallelBehaviour.WHEN_ALL);
+                    //par.addSubBehaviour(new MnrAgentContractNet (myAgent, msg2));
+                    //ACLMessage msg3 = blockingReceive(MessageTemplate.MatchSender(ax));
+                    System.out.println("Behavior created");
                 }
 
                 // response
