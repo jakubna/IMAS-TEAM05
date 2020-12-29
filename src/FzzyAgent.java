f7651e6cdfb1ba16fab5695bb23c25aa902e22b5
Eric Roselló <roselloeric21@gmail.com>
2020-12-29 19:55:13 +0100

    Changed cyclic behaviour to simple, to avoid error with contractnet

--- src/FzzyAgent.java
+++ src/FzzyAgent.java
@@ -29,16 +29,16 @@ public class FzzyAgent extends Agent {
             super(a, mt);
             Object[] args = getArguments();
             String name = args[0].toString();
-            System.out.println("-> "+myAgent.getName()+": Has been created with configuration "+name);
+            //System.out.println("-> "+myAgent.getName()+": Has been created with configuration "+name);
         }
 
         @Override
         protected ACLMessage handleCfp(ACLMessage cfp) throws RefuseException, FailureException, NotUnderstoodException {
-            System.out.println("Agent "+getLocalName()+": CFP received from "+cfp.getSender().getName()+". Action is "+cfp.getContent());
+            //System.out.println("Agent "+getLocalName()+": CFP received from "+cfp.getSender().getName()+". Action is "+cfp.getContent());
             int proposal = (int) (Math.random() * 10);
             if (proposal > 2) {
                 // We provide a proposal
-                System.out.println("Agent "+getLocalName()+": Proposing "+proposal);
+                //System.out.println("Agent "+getLocalName()+": Proposing "+proposal);
                 ACLMessage propose = cfp.createReply();
                 propose.setPerformative(ACLMessage.PROPOSE);
                 propose.setContent(String.valueOf(proposal));
@@ -46,39 +46,50 @@ public class FzzyAgent extends Agent {
             }
             else {
                 // We refuse to provide a proposal
-                System.out.println("Agent "+getLocalName()+": Refuse");
+                //System.out.println("Agent "+getLocalName()+": Refuse");
                 throw new RefuseException("evaluation-failed");
             }
         }
 
         @Override
         protected ACLMessage handleAcceptProposal(ACLMessage cfp, ACLMessage propose, ACLMessage accept) throws FailureException {
-            System.out.println("Agent "+getLocalName()+": Proposal accepted");
+            //System.out.println("Agent "+getLocalName()+": Proposal accepted");
             //codicio temporal
             if (cfp != null) {
-                System.out.println("Agent "+getLocalName()+": Action successfully performed");
+                //System.out.println("Agent "+getLocalName()+": Action successfully performed");
                 ACLMessage inform = accept.createReply();
                 inform.setPerformative(ACLMessage.INFORM);
                 return inform;
             }
             else {
-                System.out.println("Agent "+getLocalName()+": Action execution failed");
+                //System.out.println("Agent "+getLocalName()+": Action execution failed");
                 throw new FailureException("unexpected-error");
             }
         }
 
         @Override
         protected void handleRejectProposal(ACLMessage cfp, ACLMessage propose, ACLMessage reject) {
-            System.out.println("Agent "+getLocalName()+": Proposal rejected");
+            //System.out.println("Agent "+getLocalName()+": Proposal rejected");
+        }
+
+        @Override
+        public int onEnd() {
+            //System.out.println("Agent "+getLocalName()+": RESETING");
+            reset();
+            return super.onEnd();
         }
     }
 
 
     protected void setup() {
+        Object[] args = getArguments();
+        String type = args[0].toString();
+
         // Registration with the DF
         DFAgentDescription dfd = new DFAgentDescription();
         ServiceDescription sd = new ServiceDescription();
         sd.setType("FuzzyAgent");
+        sd.setType(type);
         sd.setName(getName());
         // sd.setOwnership("TEAM05");
         dfd.setName(getAID());
